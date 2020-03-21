aef86eb339b7f56508085efc7257a0397b3078b3
David Würfel <david.wuerfel@googlemail.com>
2020-03-21 14:57:50 +0100

    feat: generate first 3 pages

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -13,6 +13,11 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ErrorComponent } from './layouts/error/error.component';
+import { CompanyPageComponent } from './pages/company-page/company-page.component';
+import { CompanyOfferPageComponent } from './pages/company-offer-page/company-offer-page.component';
+import { UserProfilePageComponent } from './pages/user-profile-page/user-profile-page.component';
+
+const PAGES = [CompanyPageComponent, CompanyOfferPageComponent, UserProfilePageComponent];
 
 @NgModule({
   imports: [
@@ -24,7 +29,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     SharinghumanresourcesEntityModule,
     SharinghumanresourcesAppRoutingModule
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent, PAGES],
   bootstrap: [MainComponent]
 })
 export class SharinghumanresourcesAppModule {}
---
+++ src/main/webapp/app/pages/company-offer-page/company-offer-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-company-offer-page',
+  templateUrl: './company-offer-page.component.html',
+  styleUrls: ['./company-offer-page.component.scss']
+})
+export class CompanyOfferPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
