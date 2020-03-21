3c3817d62377e23d54abc2474225ec7298e9a56f
David Würfel <david.wuerfel@googlemail.com>
2020-03-21 14:42:53 +0100

    feat: initial avatar component

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+import { AvatarComponent } from './components/avatar/avatar.component';
 
 @NgModule({
   imports: [SharinghumanresourcesSharedLibsModule],
-  declarations: [AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
+  declarations: [AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective, AvatarComponent],
   entryComponents: [LoginModalComponent],
   exports: [SharinghumanresourcesSharedLibsModule, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective]
 })
