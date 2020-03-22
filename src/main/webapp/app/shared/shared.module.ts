e8df1032507ed51f022011f899d7d1718904e8a8
David Würfel <david.wuerfel@googlemail.com>
2020-03-22 12:22:26 +0100

    feat: add control components

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,11 +5,34 @@ import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
 import { AvatarComponent } from './components/avatar/avatar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { UploadPhotoComponent } from './components/upload-photo/upload-photo.component';
+import { CompetenceListEntryComponent } from './components/competence-list-entry/competence-list-entry.component';
+import { OfferListEntryComponent } from './components/offer-list-entry/offer-list-entry.component';
+
+const CONTROL_COMPONENTS = [
+  AvatarComponent,
+  ButtonComponent,
+  SearchBarComponent,
+  NavigationBarComponent,
+  OfferListEntryComponent,
+  UploadPhotoComponent,
+  CompetenceListEntryComponent
+];
 
 @NgModule({
   imports: [SharinghumanresourcesSharedLibsModule],
-  declarations: [AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective, AvatarComponent],
+  declarations: [AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective, CONTROL_COMPONENTS],
   entryComponents: [LoginModalComponent],
-  exports: [SharinghumanresourcesSharedLibsModule, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective, AvatarComponent]
+  exports: [
+    SharinghumanresourcesSharedLibsModule,
+    AlertComponent,
+    AlertErrorComponent,
+    LoginModalComponent,
+    HasAnyAuthorityDirective,
+    CONTROL_COMPONENTS
+  ]
 })
 export class SharinghumanresourcesSharedModule {}
