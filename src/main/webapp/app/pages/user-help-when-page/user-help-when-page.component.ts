c9233bb143b7ab3b532c3336034213abc5ff5223
David Würfel <david.wuerfel@googlemail.com>
2020-03-22 12:46:24 +0100

    feat: add when page

--- src/main/webapp/app/pages/user-help-when-page/user-help-when-page.component.ts
+++ src/main/webapp/app/pages/user-help-when-page/user-help-when-page.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'jhi-user-help-when-page',
   templateUrl: './user-help-when-page.component.html',
   styleUrls: ['./user-help-when-page.component.scss']
 })
-export class UserHelpWhenPageComponent implements OnInit {
-  constructor() {}
+export class UserHelpWhenPageComponent {
+  public availableDate: Date | undefined;
 
-  ngOnInit(): void {}
+  constructor(private router: Router) {}
+
+  public get canContinue(): boolean {
+    return this.availableDate !== undefined;
+  }
+
+  public continue(): void {
+    const message = `User can help on ${this.availableDate}.`;
+    // TODO: wuerfelda: Remove, just for testing purposes.
+    // eslint-disable-next-line no-console
+    console.log(message);
+    this.router.navigate(['created']);
+  }
 }
