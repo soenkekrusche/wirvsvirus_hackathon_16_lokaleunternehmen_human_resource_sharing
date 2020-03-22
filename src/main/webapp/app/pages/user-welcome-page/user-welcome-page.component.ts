53a25112db7c11d79ed6862717e4f8018ab39470
David Würfel <david.wuerfel@googlemail.com>
2020-03-22 12:32:47 +0100

    feat: initial layout and data for welcome page

--- src/main/webapp/app/pages/user-welcome-page/user-welcome-page.component.ts
+++ src/main/webapp/app/pages/user-welcome-page/user-welcome-page.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'jhi-user-welcome-page',
   templateUrl: './user-welcome-page.component.html',
   styleUrls: ['./user-welcome-page.component.scss']
 })
-export class UserWelcomePageComponent implements OnInit {
-  constructor() {}
+export class UserWelcomePageComponent {
+  public firstName = '';
+  public lastName = '';
+  public address = '';
+  public image = '';
 
-  ngOnInit(): void {}
+  public get canContinue(): boolean {
+    return this.firstName !== '' && this.lastName !== '' && this.address !== '';
+  }
+
+  public continue(): void {
+    const message = `Welcome ${this.firstName} ${this.lastName} located in ${this.address}.`;
+    // TODO: wuerfelda: Remove, just for testing purposes.
+    // eslint-disable-next-line no-console
+    console.log(message);
+  }
 }
