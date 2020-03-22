bc95704367b48df642fb502fb01a83ec9e97b36e
David Würfel <david.wuerfel@googlemail.com>
2020-03-22 12:24:34 +0100

    feat: add some routes

--- src/main/webapp/app/pages/pages-routing.module.ts
+++ src/main/webapp/app/pages/pages-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CompanyOfferPageComponent } from './company-offer-page/company-offer-page.component';
+import { UserWelcomePageComponent } from './user-welcome-page/user-welcome-page.component';
+import { UserHelpHowPageComponent } from './user-help-how-page/user-help-how-page.component';
+import { UserProfileCreatedPageComponent } from './user-profile-created-page/user-profile-created-page.component';
+import { UserHelpWhenPageComponent } from './user-help-when-page/user-help-when-page.component';
 
 const ROUTES: Routes = [
   {
@@ -10,6 +14,38 @@ const ROUTES: Routes = [
     data: {
       authorities: [],
       pageTitle: 'Angebote'
+    }
+  },
+  {
+    path: 'welcome',
+    component: UserWelcomePageComponent,
+    data: {
+      authorities: [],
+      pageTitle: 'Wer bist du?'
+    }
+  },
+  {
+    path: 'how',
+    component: UserHelpHowPageComponent,
+    data: {
+      authorities: [],
+      pageTitle: 'Wie kannst du helfen?'
+    }
+  },
+  {
+    path: 'when',
+    component: UserHelpWhenPageComponent,
+    data: {
+      authorities: [],
+      pageTitle: 'Wann kannst du helfen?'
+    }
+  },
+  {
+    path: 'created',
+    component: UserProfileCreatedPageComponent,
+    data: {
+      authorities: [],
+      pageTitle: 'Wann kannst du helfen?'
     }
   }
 ];
